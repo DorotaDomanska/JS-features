cc22982e0838af67be6d82999d01751a986cc39a
DorotaDomanska <dorota@domanska.eu>
2022-11-18 10:23:58 +0100

    01, 02, 03

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,26 @@
-// Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
-// Change code below this line
 
 console.log(galleryItems);
+
+console.log(galleryItems);
+
+const list = document.querySelector('.gallery');
+
+const item = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `
+         <a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+        `
+  )
+  .join('');
+
+list.insertAdjacentHTML('afterbegin', item);
+
+let lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+});
